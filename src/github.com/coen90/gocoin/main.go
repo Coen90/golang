9ee3108bf45fd0f10094882ca0c4fcb12b1fa167
Hyuntae <bht9011@gmail.com>
2021-11-11 23:43:32 +0900

    study after runing :)

--- src/github.com/coen90/gocoin/main.go
+++ src/github.com/coen90/gocoin/main.go
@@ -6,33 +6,71 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/coen90/gocoin/blockchain"
 	"github.com/coen90/gocoin/utils"
 )
 
 const port string = ":4000"
 
+type URL string 
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
 type URLDescription struct {
-	URL string
-	Method string
-	Description string
+	URL URL `json:"url"`
+	Method string `json:"method"`
+	Description string `json:"description"`
+	Payload string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL: "/",
+			URL: URL("/"),
 			Method: "GET",
 			Description: "See Documentation",
 		},
+		{
+			URL: URL("/blocks/{id}"),
+			Method: "POST",
+			Description: "Add A Block",
+			Payload: "data:string",
+		},
+		{
+			URL: URL("/blocks/{id}"),
+			Method: "POST",
+			Description: "See A Block",
+		},
 	}
-	b, err := json.Marshal(data)
-	utils.HandleErr(err)
-	fmt.Printf("%s", b)
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
 }
+
+func blocks(rw http.ResponseWriter, r * http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		fmt.Println(addBlockBody)
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+} 
 
 func main() {
 	http.HandleFunc("/", documentation)
-	fmt.Printf("Listening on http://localhost%s", port)
+	http.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
